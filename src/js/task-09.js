13919bf102f8fda9f3a58a4160a524178108d2b2
Jarek Dzwolak <jaroslawdzwolak@gmail.com>
2023-03-23 18:16:31 +0100

    task9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const spanText = document.querySelector(".color");
+const btnEl = document.querySelector(".change-color");
+
+btnEl.addEventListener("click", () => {
+  const Color = getRandomHexColor();
+  document.body.style.backgroundColor = Color;
+  spanText.textContent = Color;
+});
