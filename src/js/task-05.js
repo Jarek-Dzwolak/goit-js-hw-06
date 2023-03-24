0aad820c79833b7781251c6d723a16460b66c9ae
Jarek Dzwolak <jaroslawdzwolak@gmail.com>
2023-03-24 16:59:00 +0100

    changes in 5

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -2,5 +2,7 @@ const myInput = document.getElementById("name-input");
 const myOutput = document.getElementById("name-output");
 
 myInput.addEventListener("input", (event) => {
-  myOutput.textContent = event.currentTarget.value;
+  event.currentTarget.value
+    ? (myOutput.textContent = event.currentTarget.value)
+    : (myOutput.textContent = "Anonymous");
 });
